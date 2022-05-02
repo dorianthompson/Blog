0d0a02c9990c1a5986747d2493e947883a3a8844
Dorian Thompson <thompsondorian2@gmail.com>
2022-05-02 14:13:56 -0400

    set up network request from redux using redux-thunk

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
+export const fetchPosts = () => {
+    return async (dispatch) => {
+        const response = await jsonPlaceHolder.get('/posts');
+
+        dispatch({ 
+            type: 'FETCH_POSTS',
+            payload: response
+        })
+    }
+};
+
