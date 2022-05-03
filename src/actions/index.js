4ee1fb6e617c84c050b77ecd10759fa7fdea37f7
Dorian Thompson <thompsondorian2@gmail.com>
2022-05-03 14:35:27 -0400

    add UserHeader component and usersReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,22 @@
+import _ from 'lodash';
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
+export const fetchPostsAndUsers = () => {
+    return async (dispatch, getState) => {
+        await dispatch(fetchPosts());
+        
+        //lodash uniqueId and map function
+        /*const userIds = _.uniq(_.map(getState().posts, 'userId'));
+        userIds.forEach(id => dispatch(fetchUser(id)));*/
+
+        //lodash chain function to combine the above commented out methods
+        _.chain(getState().posts)
+            .map('userId')
+            .uniq()
+            .forEach(id => dispatch(fetchUser(id)))
+            .value();
+    }
+};
 
 export const fetchPosts = () => {
     return async (dispatch) => {
@@ -11,3 +29,24 @@ export const fetchPosts = () => {
     }
 };
 
+export const fetchUser = (id) => {
+    return async (dispatch) => {
+        const response = await jsonPlaceHolder.get(`/users/${id}`);
+    
+        dispatch({
+            type: 'FETCH_USER',
+            payload: response.data
+        })
+    }
+};
+
+//memoized network request so we avoid making mutltiple request for the same user
+/*const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    
+
+        dispatch({
+            type: 'FETCH_USER',
+            payload: response.data
+        })
+});*/
